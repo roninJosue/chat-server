cdf1dd53ff25fbbcd41ab330f9adb915be602b3c
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-08-30 16:05:28 -0600

    feat: Add leave_room and disconnect event

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cors = require('cors')
 const {Server} = require('socket.io')
 const harperSaveMessage = require('./services/harper-save-message')
 const harperGetMessages = require('./services/harper-get-messages')
+const leaveRoom = require('./utils/leave-room')
 
 app.use(cors())
 
@@ -16,7 +17,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3001',
     methods: ['GET', 'POST']
   }
 })
@@ -67,6 +68,30 @@ io.on('connection', (socket) => {
     harperSaveMessage(message, username, room, __createdtime__)
       .then(response => console.log(response))
       .catch(err => console.log(err))
+  })
+
+  socket.on('leave_room', data => {
+    const {room, username} = data
+    socket.leave(room)
+    const __createdtime__ = Date.now()
+    allUsers = leaveRoom(socket.id, allUsers)
+    socket.to(room).emit('chatroom_users', allUsers)
+    socket.to(room).emit('receive_message', {
+      username: CHAT_BOT,
+      message: `${username} has left the chat`,
+      __createdtime__,
+    })
+  })
+
+  socket.on('disconnect', () => {
+    const user = allUsers.find(usr => usr.id === socket.id)
+    if (user?.username){
+      allUsers = leaveRoom(socket.id, allUsers)
+      socket.to(chatRoom).emit('chatroom_users', allUsers)
+      socket.to(chatRoom).emit('receive_message', {
+        message: `${user.username} has disconnected from the chat`
+      })
+    }
   })
 
 })
